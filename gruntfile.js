f29e2a140fd6bec26f23f0063e964a149eb3de2c
Erik Engervall <erik.engervall@gmail.com>
2016-07-15 02:54:57 +0200

    added bower support, updated name, sorted blank space issue and added extra settings

--- gruntfile.js
+++ gruntfile.js
@@ -1,26 +1,37 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
+
+    /**
+     * Define our package
+     * @type {Object}
+     */
     pkg : grunt.file.readJSON('package.json'),
 
-    jshint: {
-      all: ['src/js/*.js']
-    },
-
+    /**
+     * Compile LESS into CSS
+     * @type {Object}
+     */
     less: {
       build: {
         options: {
           paths: ["css"]
         },
         files: {
-          "dist/css/<%= pkg.name %>.css": "src/less/<%= pkg.name %>.less"
+          "dist/css/<%= pkg.name %>.css" : "src/less/<%= pkg.name %>.less",
+          "examples/css/demo.css" : "src/less/demo.less"
         }
       }
     },
 
+    /**
+     * Compress JS
+     * @type {Object}
+     */
     uglify: {
       build: {
         src: 
-          'src/js/*.js'
+          'src/js/<%= pkg.name %>.js'
         ,
         dest: 
           'dist/js/<%= pkg.name %>.min.js'
@@ -28,19 +39,49 @@ module.exports = function(grunt) {
       }
     },
 
+    /**
+     * Compress CSS
+     * @type {Object}
+     */
     cssmin: {
       build: {
-        files: [{
-          expand: true,
-          cwd: 'dist/css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'dist/css',
-          ext: '.min.css'
-        }]
+        files: [
+          {
+            expand: true,
+            cwd: 'dist/css',
+            src: [
+              '*.css',
+              '!*.min.css'
+            ],
+            dest: 'dist/css',
+            ext: '.min.css'
+          }
+        ]
       }
     },
 
-    watch : {
+    /**
+     * Copy JS source
+     * @type {Object}
+     */
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: "./src/js/",
+            src: "<%= pkg.name %>.js",
+            dest: "./dist/js/"
+          }
+        ]
+      }
+    },
+
+    /**
+     * Watch tasks array for changes
+     * @type {Object}
+     */
+    watch: {
       files: [
         "./src/less/*",
         "./src/js/*"
@@ -48,15 +89,31 @@ module.exports = function(grunt) {
       tasks: [
         "less",
         "uglify",
-        "cssmin"
+        "cssmin",
+        "copy"
       ]
     },
 
   });
+
+  /**
+   * Load npm tasks
+   */
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  /**
+   * Register task
+   */
   grunt.registerTask('default', 'watch');
+
 };
+
+
+
+
+
+
